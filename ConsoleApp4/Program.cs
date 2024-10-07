ee9006d28456c758bb18c5170fb3e2d16a8c8d07
exute <exute@DESKTOP-38P5TV8>
2024-10-07 23:19:31 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,178 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //задача 1
+            // Console.WriteLine("введите первое число:");
+            //int a = int.Parse(Console.ReadLine());
+            //Console.WriteLine("Введите второе число:");
+            //int b = int.Parse(Console.ReadLine());
+            //int c = a + b;
+            //int v = a * b;
+            //Console.WriteLine($"{c}");
+            //Console.WriteLine($"{v}");
+            //Console.ReadKey();
+
+            //задача 2
+            //Console.WriteLine("Введите число a=");
+            //int a = int.Parse(Console.ReadLine());
+            //Console.WriteLine("Введите число b=");
+            //int b = int.Parse(Console.ReadLine());
+            //Console.WriteLine($"{a} + {b} = {b} + {a}");
+            //Console.WriteLine($"{a} * {b} = {b} * {a}");
+            //Console.ReadKey();
+
+            //задача 3
+            //Console.WriteLine("Введите первое число:");
+            //double a = Int32.Parse(Console.ReadLine());
+            //Console.WriteLine("Введите второе число:");
+            //double b = Int32.Parse(Console.ReadLine());
+            //Console.WriteLine("Введите третье число:");
+            //double c = Int32.Parse(Console.ReadLine());
+            //double p = a + b + c;
+            //Console.WriteLine($"{a} + {b} + {c} = {p}");
+            //Console.ReadKey();
+
+            //задача 4
+            //Console.WriteLine("Введите первое число");
+            //double a = double.Parse(Console.ReadLine());
+            //Console.WriteLine("Введите второе число");
+            //double b = double.Parse(Console.ReadLine());
+            //double p = a * b;
+            //Console.WriteLine($"{a:f1} * {b:f1} = {p:f1}");
+            //Console.ReadKey();
+
+            //Задача 5
+            // Console.WriteLine("Введите первое число");
+            //double a = double.Parse(Console.ReadLine());
+            //Console.WriteLine("Введите второе число");
+            //double b = double.Parse(Console.ReadLine());
+            //Console.WriteLine("Введите третье число");
+            //double c = double.Parse(Console.ReadLine());
+            //double p = a / b;
+            //Console.WriteLine($"{a:f3}/{b:f3}={p}");
+            //Console.ReadKey();
+
+            //задача 6 
+            //Console.WriteLine("Введите первое число");
+            //double a = double.Parse(Console.ReadLine());
+            //Console.WriteLine("Введите второе число");
+            //double b = double.Parse(Console.ReadLine());
+            //Console.WriteLine("Введите третье число");
+            //double c = double.Parse(Console.ReadLine());
+            //Console.WriteLine($"({a:f2} + {b:f2}) + {c:f3} = {a:f1} + ({b:f3} + {c:f2})");
+            //Console.ReadKey();
+
+            // задача 7
+            //Console.WriteLine("Введите первое число");
+            //int a = int.Parse(Console.ReadLine());
+            //Console.WriteLine("Введите второе число");
+            //int b = int.Parse(Console.ReadLine());
+            //int a1 = a + b;
+            //int b1 = a - b;
+            //int c1 = a * b;
+            //Console.WriteLine($"a+b={a}+{b}={a1} \t a-b={a}-{b}={b1} \t a*b={a}*{b}={c1}");
+            //Console.ReadKey();
+
+            //задача 8
+            //Console.WriteLine("Номинал купюры=");
+            //int a  = int.Parse(Console.ReadLine());
+            //int b = 23;
+            //Console.WriteLine($"колво купюр = {b}");
+            //int c = a * b;
+            //Console.WriteLine($"Сумма денег = {c:f2}");
+            //Console.ReadKey();
+
+
+
+            //часть 2 задание 1
+            //Console.Write("R=");
+            //double R = Convert.ToDouble(Console.ReadLine());
+            //Console.WriteLine($"L = {2 * Math.PI * R} \n S={ Math.PI * Math.Pow(R, 2)}");
+            //задание 2
+            //Console.WriteLine("население =");
+            //int a = int.Parse(Console.ReadLine());
+            //Console.WriteLine("площадь=");
+            //int b = int.Parse(Console.ReadLine());
+            //int p = a / b;
+            //Console.WriteLine($"{a}/{b} = {p}");
+            //Console.ReadKey();
+
+            //задача 3
+            //Console.Write("первый катет = ");
+            //int a = int.Parse(Console.ReadLine());
+            //Console.Write("второй катет = ");
+            //int b = int.Parse(Console.ReadLine());
+            //// - 
+            ////double a1 = (Math.Pow((a),2) + Math.Pow((b), 2 ));
+            //// - 
+            //int a2 = 2 * a + b;
+            //Console.WriteLine($"{a2}");
+            //Console.ReadKey();
+
+            //Задача 4 
+            //Console.Write("первоя сторона = ");
+            //int a = int.Parse(Console.ReadLine());
+            //Console.Write("второй сторона = ");
+            //int b = int.Parse(Console.ReadLine());
+            //int a1 = a + a + b + b;
+            //double a2 = (Math.Pow((a), 2) + Math.Pow((b), 2));
+            //Console.WriteLine($"p={a1} \n диагональ={a2}");
+            //Console.ReadKey();
+
+            //задача 5
+            //Console.Write("первоя координата x1 = ");
+            //double x1 = double.Parse(Console.ReadLine());
+            //Console.Write("вторую координата y1 = ");
+            //double y1 = double.Parse(Console.ReadLine());
+            //Console.Write("первую координата x2 = ");
+            //double x2 = double.Parse(Console.ReadLine());
+            //Console.Write("вторую координата y2 = ");
+            //double y2 = double.Parse(Console.ReadLine());
+            //double a = Math.Sqrt(Math.Pow((x2 - x1), 2) + Math.Pow((y2 - y1), 2));
+            //Console.WriteLine($"Расстояние между точками: {a}");
+            //Console.ReadKey();
+
+            //задача 6
+            //Console.WriteLine("Стоимость 1 кг конфет = 5 рублей");
+            //int a = 5;
+            //Console.WriteLine("Стоимость печенья  = 6 рублей ");
+            //int b = 6;
+            //Console.WriteLine("Стоимость яблок = 7 рублей");
+            //int c = 7;
+            //Console.WriteLine("сколько кг конфет = ");
+            //int a1 = int.Parse(Console.ReadLine());
+            //int a2 = a + a1;
+            //Console.WriteLine("сколько кг печенья = ");
+            //int b1 = int.Parse(Console.ReadLine());
+            //int b2 = b + b1;
+            //Console.WriteLine("сколько кг яблок = ");
+            //int c1 = int.Parse(Console.ReadLine());
+            //int c2 = c + c1;
+            //int c13 = a2 + b2 + c2;
+            //Console.WriteLine($"стоимость всей покупки ={c13} рублей");
+            //Console.ReadKey();
+
+            //Задача 7
+            //Console.Write("Введите двузначное число: ");
+            //int a = int.Parse(Console.ReadLine());
+            //int a1 = a / 10;
+            //int a2 = a % 10;
+            //int a3 = a1 + a2;
+            //int a4 = a1 * a2;
+            //Console.WriteLine($"Число десятков: {a1}");
+            //Console.WriteLine($"Число единиц: {a2}");
+            //Console.WriteLine($"Сумма цифр: {a3}");
+            //Console.WriteLine($"Произведение цифр: {a4}");
+            //Console.ReadKey();
+        }
+    }
+}
